c8ac1a355aa794992c1e235e25a9cfae87742642
Corey McCue <coreysmccue@gmail.com>
2021-02-16 16:35:42 +0100

    clean working tree

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
    get '/tasks/new', to: "tasks#new", as: :new_task
   #   # post information into DB
    post '/tasks', to: "tasks#create"
-    # # see one restaurant
+    # # see one task
    get '/tasks:id/', to: "tasks#show", as: :task
+    # update a restaurant
+    # display the form
+  get '/tasks:id/edit', to: "tasks#edit", as: :edit
+    # post information into db
+  patch '/tasks:id', to: "tasks#update"
 end
